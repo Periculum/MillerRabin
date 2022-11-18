e5717eb5978f97c4ec525c2be963272e4cc1cb0b
Wilhelm Drehling <wilhelmdrehling@web.de>
2022-11-18 10:46:04 +0100

    Create Fermat.py
    Small Fermat primality test. The test fails against Carmichael-Numbers, that's why you should use the Miller-Rabin-Test

---
+++ Fermat.py
@@ -0,0 +1,27 @@
+# Fermattest
+from random import randrange
+
+
+def isPrime(n):
+    a = randrange(2, n)
+    return pow(a, n - 1, n) == 1
+
+# this method runs 1000 times the Fermattest and counts the amount of True and False
+# it returns a quote where 0 stands for prime, and 100 for not prime
+# everthing inbetween means there are some false positives (so be careful with the final result)
+# Carmichael-Numbers like 561 or 8911 generate usually a lot of false positives
+def testQuote(tries, n):
+    z = 0
+    for i in range(tries):
+        if not isPrime(n):
+            z += 1
+    return z / tries * 100 
+
+
+def main():
+    n = input("Please insert an uneven Number: ")
+    print(testQuote(1000, int(n)))
+
+
+if __name__ == '__main__':
+    main()
