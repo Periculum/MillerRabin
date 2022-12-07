ce93b4d30f49b16dadf5f2ce40f67cd5bd16f765
Wilhelm Drehling <wilhelmdrehling@web.de>
2022-12-07 11:40:37 +0100

    Changed quote
    some small stuff

--- Fermat.py
+++ Fermat.py
@@ -7,7 +7,7 @@ def isPrime(n):
     return pow(a, n - 1, n) == 1
 
 # this method runs 1000 times the Fermattest and counts the amount of True and False
-# it returns a quote where 0 stands for prime, and 100 for not prime
+# it returns a quote where 0 stands for prime, and 1.0 for not prime
 # everthing inbetween means there are some false positives (so be careful with the final result)
 # Carmichael-Numbers like 561 or 8911 generate usually a lot of false positives
 def testQuote(tries, n):
@@ -15,7 +15,7 @@ def testQuote(tries, n):
     for i in range(tries):
         if not isPrime(n):
             z += 1
-    return z / tries * 100 
+    return z / tries 
 
 
 def main():
